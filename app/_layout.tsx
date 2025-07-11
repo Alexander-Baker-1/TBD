e55657bb5451a5f708837750a6f89d639ee2467a
Alexander Baker <Alexander.Baker-1@colorado.edu>
2025-07-11 11:43:53 -0600

    Cleaned up code

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,14 @@
 import { Stack } from 'expo-router';
+import { StatusBar } from 'expo-status-bar';
+import NavigationBar from 'expo-navigation-bar';
+import { useEffect } from 'react';
+import { Platform } from 'react-native';
 
 export default function RootLayout() {
   return (
-    <Stack>
-      <Stack.Screen name="(tabs)" />
+    <Stack screenOptions={{headerShown: false}}>
+      <Stack.Screen name="(pages)" />
+      <Stack.Screen name="+not-found" />
     </Stack>
   );
 }
