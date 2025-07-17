7306ca67b0316b09e2f79542bd7f2f20dd14da47
Alexander Baker <Alexander.Baker-1@colorado.edu>
2025-07-16 18:09:22 -0700

    Fix config error

--- app.config.js
+++ app.config.js
@@ -2,23 +2,42 @@ import 'dotenv/config';
 
 export default {
   expo: {
-    name: "TBD",
-    slug: "TBD",
-    scheme: "tbd",
-    version: "1.0.0",
-    plugins: [
-      "expo-router",
-    ],
+    name: process.env.EXPO_PUBLIC_APP_NAME,
+    slug: process.env.EXPO_PUBLIC_APP_SLUG,
+    scheme: process.env.EXPO_PUBLIC_APP_SCHEME,
+    version: process.env.EXPO_PUBLIC_APP_VERSION,
     orientation: "portrait",
+    userInterfaceStyle: "automatic",
     icon: "./assets/images/icon.png",
-    userInterfaceStyle: "light",
     splash: {
       image: "./assets/images/splash-icon.png",
       resizeMode: "contain",
-      backgroundColor: "#ffffff"
+      backgroundColor: "#ffffff",
+      dark: {
+        image: "./assets/images/splash-icon-dark.png",
+        backgroundColor: "#000000"
+      },
+      imageWidth: 200
     },
-    assetBundlePatterns: [
-      "**/*"
+    assetBundlePatterns: ["**/*"],
+    newArchEnabled: true,
+    experiments: {
+      typedRoutes: true
+    },
+    plugins: [
+      "expo-router",
+      [
+        "expo-splash-screen",
+        {
+          backgroundColor: "#232323",
+          image: "./assets/images/splash-icon.png",
+          dark: {
+            image: "./assets/images/splash-icon-dark.png",
+            backgroundColor: "#000000"
+          },
+          imageWidth: 200
+        }
+      ]
     ],
     ios: {
       supportsTablet: true,
@@ -28,25 +47,29 @@ export default {
     },
     android: {
       package: process.env.EXPO_PUBLIC_APPWRITE_PACKAGE_NAME,
+      runtimeVersion: "1.0.0",
+      edgeToEdgeEnabled: true,
       adaptiveIcon: {
         foregroundImage: "./assets/images/adaptive-icon.png",
         backgroundColor: "#ffffff"
-      },
-      runtimeVersion: "1.0.0"
+      }
     },
     web: {
-      favicon: "./assets/images/favicon.png"
+      favicon: "./assets/images/favicon.png",
+      bundler: "metro",
+      output: "static"
     },
     updates: {
       url: `https://u.expo.dev/${process.env.EXPO_PUBLIC_EAS_PROJECT_ID}`
     },
     extra: {
-      EXPO_PUBLIC_APPWRITE_ENDPOINT: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
-      EXPO_PUBLIC_APPWRITE_PROJECT_ID: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
-      EXPO_PUBLIC_APPWRITE_PACKAGE_NAME: process.env.EXPO_PUBLIC_APPWRITE_PACKAGE_NAME,
       eas: {
         projectId: process.env.EXPO_PUBLIC_EAS_PROJECT_ID
-      }
-    }
+      },
+      EXPO_PUBLIC_APPWRITE_ENDPOINT: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
+      EXPO_PUBLIC_APPWRITE_PROJECT_ID: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
+      EXPO_PUBLIC_APPWRITE_PACKAGE_NAME: process.env.EXPO_PUBLIC_APPWRITE_PACKAGE_NAME
+    },
+    owner: process.env.EXPO_PUBLIC_EXPO_OWNER
   }
 };
