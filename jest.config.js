002cf7f50b7a903d66a5fe7875e8d3cb6daca37f
Alexander Baker <Alexander.Baker-1@colorado.edu>
2025-07-16 19:47:04 -0700

    Fix config error

--- jest.config.js
+++ jest.config.js
@@ -1,36 +1,16 @@
 module.exports = {
-  testEnvironment: 'node',
-  testTimeout: 60000,
-  maxWorkers: 1,
+  preset: 'jest-expo',
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
-  testMatch: [
-    '**/__tests__/**/*.test.js',
-    '**/__tests__/**/*.spec.js'
-  ],
-  testPathIgnorePatterns: [
-    '/node_modules/',
-    '/build/',
-    '/dist/',
-    '/coverage/',
-    'app/',
-    'components/',
-    'context/'
-  ],
-  collectCoverageFrom: [
-    'lib/**/*.{js,jsx}',
-    '!**/node_modules/**',
-    '!**/coverage/**',
-    '!**/app/**',
-    '!**/components/**',
-    '!**/context/**',
-    '!babel.config.js',
-    '!jest.config.js',
-    '!jest.setup.js'
-  ],
-  coverageDirectory: 'coverage',
-  verbose: true,
   transformIgnorePatterns: [
-    'node_modules/(?!appwrite)'
+    'node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg)'
   ],
-  moduleFileExtensions: ['js', 'json']
+  testEnvironment: 'node',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  collectCoverageFrom: [
+    '**/*.{js,jsx,ts,tsx}',
+    '!**/coverage/**',
+    '!**/node_modules/**',
+    '!**/babel.config.js',
+    '!**/jest.setup.js'
+  ]
 };
